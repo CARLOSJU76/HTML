9a4ad9835c91b1c0f215e9093443aa15a5470079
CARLOSJU76 <cajugato76@hotmail.com>
2024-06-17 18:36:09 -0500

    primera actualizacion

---
+++ JS/javas.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === slideIndex) ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+// Mostrar la primera diapositiva
+showSlide(slideIndex);
