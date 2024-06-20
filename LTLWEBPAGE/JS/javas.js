48dd36dcdcbf4b9b427b794ead098b58331c2f46
CAJUGATO76 <cajugato76@hotmail.com>
2024-06-20 14:25:03 -0500

    actualización 20/06/24 2:24

--- LTLWEBPAGE/JS/javas.js
+++ LTLWEBPAGE/JS/javas.js
@@ -4,9 +4,10 @@ function showSlide(index) {
     const slides = document.querySelectorAll('.slide');
     if (index >= slides.length) {
         slideIndex = 0;
-    }
-    if (index < 0) {
+    }else if (index < 0) {
         slideIndex = slides.length - 1;
+    }else{
+        slideIndex=index;
     }
     slides.forEach((slide, i) => {
         slide.style.display = (i === slideIndex) ? 'block' : 'none';
@@ -25,3 +26,6 @@ function prevSlide() {
 
 // Mostrar la primera diapositiva
 showSlide(slideIndex);
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(slideIndex);
+});
